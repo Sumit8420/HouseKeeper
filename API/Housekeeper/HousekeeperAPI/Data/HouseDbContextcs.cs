3feb5233409c8f2f7daa0e02c19bd94140053c07
Sumit8420 <sumitkumarsingh495@gmail.com>
2022-06-23 12:42:04 +0530

    first commit

---
+++ API/Housekeeper/HousekeeperAPI/Data/HouseDbContextcs.cs
@@ -0,0 +1,18 @@
+﻿using HouseKeppingApi.Models; //*
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseKeppingApi.Data
+{
+    public class HouseDbContext : DbContext
+    {
+        public HouseDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //DbSet
+
+        public DbSet<HouseKeeping> Housekeepings { get; set; }
+
+
+    }
+}
